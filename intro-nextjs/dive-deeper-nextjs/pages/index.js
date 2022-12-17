5e0d8104f5d8550cebca501f9d117d22f7a68e14
Ronel Tayawa <lenor11326@hotmail.com>
2022-12-17 19:17:27 +0800

    application of ssg and ssr

---
+++ intro-nextjs/dive-deeper-nextjs/pages/index.js
@@ -0,0 +1,48 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some Address 5, 12345 some City",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Some Address 5, 12345 some City",
+    description: "This is a Second meetup",
+  },
+];
+
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   //fetch data from an API
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  //fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 1,
+  };
+}
+
+export default HomePage;
